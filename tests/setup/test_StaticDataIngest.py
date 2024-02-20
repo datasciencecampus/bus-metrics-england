68620ade1f40075bbf8ede2f02702058075c2ca3
Edward-Jackson-ONS <edward.jackson@ons.gov.uk>
2024-02-20 15:52:21 +0000

    refactor: test date filepath as var

--- tests/setup/test_StaticDataIngest.py
+++ tests/setup/test_StaticDataIngest.py
@@ -1,23 +1,26 @@
 """Tests for ingest_static_data module."""
 
 import pytest
-from bus_metrics.setup.ingest_static_data import StaticDataIngest
+from src.bus_metrics.setup.ingest_static_data import StaticDataIngest
 from datetime import datetime
 import os
 
 tool = StaticDataIngest()
+TESTS_DATA_PATH = os.path.join("tests", "data")
 
 
-@pytest.mark.skip(reason="Debugging one test at a time")
 def test_ingest_bus_timetable_file_exists():
     """Simple test to check that data not overwritten."""
     with pytest.raises(FileExistsError) as excinfo:
         date = datetime.now().strftime("%Y%m%d")
-        open(f"tests/data/north_east_{date}.zip", "w")
-        tool.zip_fp_root = "tests/data"
+        with open(
+            os.path.join(TESTS_DATA_PATH, f"north_east_{date}.zip"), "w"
+        ) as f:
+            f.write("")
+        tool.zip_fp_root = TESTS_DATA_PATH
         tool.ingest_bus_timetable(region="north_east")
     assert (
         str(excinfo.value)
         == "The file you are downloading to already exists (timetable)"
     )
-    os.remove(f"tests/data/north_east_{date}.zip")
+    os.remove(os.path.join(TESTS_DATA_PATH, f"north_east_{date}.zip"))
